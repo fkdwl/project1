7682fad44e95bc0568041c99353f43462d05a826
21-manic-020g <21-manic-020g@21-manic-020>
2023-10-24 16:09:04 +0900

    로그인 화면

--- Project1/src/Jpackage/Main.java
+++ Project1/src/Jpackage/Main.java
@@ -1,10 +1,29 @@
 package Jpackage;
 
-public class Main {
+import java.awt.FlowLayout;
+import java.awt.Frame;
+import java.awt.Label;
+import java.awt.TextField;
 
+public class Main {
 	public static void main(String[] args) {
-		System.out.println("hi");
-		System.out.println("bye");
+		Frame f = new Frame("login");
+		f.setSize(500, 80);
+		f.setLayout(new FlowLayout());
+		
+		Label lid = new Label("ID : " , Label.RIGHT);
+		Label lpwd = new Label("Password : ", Label.RIGHT);
+		
+		TextField id = new TextField(20);
+		TextField pwd = new TextField(20);
+		pwd.setEchoChar('*');
+		
+		f.add(lid);
+		f.add(id);
+		f.add(lpwd);
+		f.add(pwd);
+		f.setVisible(true);
+		
 	}
 
 }
