9be827a3ad02580c82015a2d2ad9f2eebecd4273
21-manic-020g <21-manic-020g@21-manic-020>
2023-10-27 10:38:55 +0900

    로그인 및 회원가입창 버튼 클릭시 확인

--- Project1/src/Jpackage/Main.java
+++ Project1/src/Jpackage/Main.java
@@ -10,6 +10,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+import javax.swing.JOptionPane;
+
 public class Main {
 	public static void main(String[] args) {
 		Frame f = new Frame("login");
@@ -32,10 +34,20 @@ public class Main {
 		f.add(lg);
 		f.add(rgs);
 
+		lg.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(f, "로그인 되었습니다.");
+
+			}
+
+		});
+
 		rgs.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				Register.main(new String[0]);
+				Register.main(null);
 			}
 		});
 
@@ -45,7 +57,7 @@ public class Main {
 				f.dispose();
 			}
 		});
-
+		f.setResizable(false);
 		f.setVisible(true);
 
 		class Register {
@@ -58,6 +70,7 @@ public class Main {
 				Label lpwd = new Label("Password : ", Label.LEFT);
 				Label lnm = new Label("Name : ");
 				Label ladr = new Label("Address : ");
+				Button rs = new Button("Register");
 
 				TextField id = new TextField(8);
 				TextField pwd = new TextField(8);
@@ -75,7 +88,9 @@ public class Main {
 
 				ladr.setBounds(50, 140, 50, 20);
 				adr.setBounds(100, 140, 150, 20);
-
+				
+				rs.setBounds(50,170,100,20);
+				
 				f.add(lid);
 				f.add(id);
 				f.add(lpwd);
@@ -84,6 +99,17 @@ public class Main {
 				f.add(nm);
 				f.add(ladr);
 				f.add(adr);
+				f.add(rs);
+
+				rs.addActionListener(new ActionListener() {
+
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						JOptionPane.showMessageDialog(f, "회원가입 되었습니다.");
+
+					}
+
+				});
 
 				f.addWindowListener(new WindowAdapter() {
 					@Override
@@ -92,6 +118,7 @@ public class Main {
 					}
 				});
 
+				f.setResizable(false);
 				f.setVisible(true);
 
 			}
--- Project1/src/Jpackage/Register.java
+++ Project1/src/Jpackage/Register.java
@@ -1,11 +1,16 @@
 package Jpackage;
 
 
+import java.awt.Button;
 import java.awt.Frame;
 import java.awt.Label;
 import java.awt.TextField;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+
+import javax.swing.JOptionPane;
 
 public class Register {
 
@@ -18,25 +23,27 @@ public class Register {
 		Label lpwd = new Label("Password : ", Label.LEFT);
 		Label lnm = new Label("Name : ");
 		Label ladr = new Label("Address : ");
+		Button rs = new Button("Register");
 
 		TextField id = new TextField(8);
 		TextField pwd = new TextField(8);
 		TextField nm = new TextField(8);
 		TextField adr = new TextField(8);
-		
-		lid.setBounds(50, 50, 50 ,20);
-		id.setBounds(100,50,150,20);
-		
-		lpwd.setBounds(50,80,50,20);
-		pwd.setBounds(100,80,150,20);
-		
-		lnm.setBounds(50,110,50,20);
-		nm.setBounds(100,110,150,20);
-		
-		ladr.setBounds(50,140,50,20);
-		adr.setBounds(100,140,150,20);
-		
 
+		lid.setBounds(50, 50, 50, 20);
+		id.setBounds(100, 50, 150, 20);
+
+		lpwd.setBounds(50, 80, 50, 20);
+		pwd.setBounds(100, 80, 150, 20);
+
+		lnm.setBounds(50, 110, 50, 20);
+		nm.setBounds(100, 110, 150, 20);
+
+		ladr.setBounds(50, 140, 50, 20);
+		adr.setBounds(100, 140, 150, 20);
+		
+		rs.setBounds(50,170,100,20);
+		
 		f.add(lid);
 		f.add(id);
 		f.add(lpwd);
@@ -45,6 +52,17 @@ public class Register {
 		f.add(nm);
 		f.add(ladr);
 		f.add(adr);
+		f.add(rs);
+
+		rs.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(f, "회원가입 되었습니다.");
+
+			}
+
+		});
 
 		f.addWindowListener(new WindowAdapter() {
 			@Override
@@ -53,6 +71,7 @@ public class Register {
 			}
 		});
 
+		f.setResizable(false);
 		f.setVisible(true);
 
 	}
