4940df9300e218b4b5a6b2a51d6c0163e2bb5865
21-manic-020g <21-manic-020g@21-manic-020>
2023-11-01 14:06:18 +0900

    자바 DB 연동

--- Project1/src/JDB/DiaryDB.java
+++ Project1/src/JDB/DiaryDB.java
@@ -1,25 +1,51 @@
 package JDB;
 
-import java.util.ArrayList;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.util.Scanner;
 
 public class DiaryDB {
 
-	public static void main(String[] args) {
-		Dao  dao1 = new Dao();
-		ArrayList<Vo>list = dao1.list();
-		
-		for(int i =0; i<list.size();i++) {
-			Vo data = (Vo)list.get(i);
-			String dept_id = data.getDept_id();
-			String dept_password = data.getDept_password();
-			String dept_name = data.getDept_name();
-			String dept_address = data.getDept_address();
-			String dept_number = data.getDept_number();
-			
-			System.out.println(dept_id + " : " +dept_password  + " : " + dept_name + " : " + dept_address + " : " + 
-			dept_number);
-		}
+	public static void Main(String[] args) {
+		String driver = "oracle.jdbc.driver.OracleDriver";
+		String url = "jdbc:oracle:thin:@localhost:1521/xe";
+		String user = "c##green";
+		String password = "green1234";
 
+		Connection con;
+
+		Scanner sc = new Scanner(System.in);
+
+		String id = sc.next();
+		String pw = sc.next();
+		String nm = sc.next();
+		String ad = sc.nextLine();
+		String nb = sc.next();
+
+		try {
+			Class.forName(driver);
+			con = DriverManager.getConnection(url, user, password);
+
+			String inserQuery = "INSERT INTO REGISTER(dept_id,dept_password,dept_name,dept_address,dept_number)"
+					+ "VALUES(?,?,?,?,?)";
+			PreparedStatement pstm = con.prepareStatement(inserQuery);
+
+			pstm.setString(1, id);
+			pstm.setString(2, pw);
+			pstm.setString(3, nm);
+			pstm.setString(4, ad);
+			pstm.setString(5, nb);
+
+			pstm.executeUpdate();
+
+			pstm.close();
+			con.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			sc.close();
+		}
 	}
 
 }
