c064a1aa9466833b658f1d012e8bbee0b71824c2
21-manic-020g <21-manic-020g@21-manic-020>
2023-11-09 15:36:56 +0900

    위치 조정

--- Project1/src/Jpackage/Tell.java
+++ Project1/src/Jpackage/Tell.java
@@ -1,5 +1,7 @@
 package Jpackage;
 
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -27,137 +29,168 @@ import javax.swing.JTextArea;
 
 public class Tell extends JFrame implements MouseListener, ActionListener, ItemListener {
 
-	Connection con;
-	PreparedStatement pstmt;
-	ResultSet rs;
+   Connection con;
+   PreparedStatement pstmt;
+   ResultSet rs;
 
-	SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-	Date now = new Date();
-	String now_dt = format.format(now);
+   SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+   Date now = new Date();
+   String now_dt = format.format(now);
 
-	public Tell() {
+   public Tell() {
 
-		JFrame f = new JFrame("알림");
-		f.setSize(400, 300);
-		f.setLayout(new FlowLayout());
+      JFrame f = new JFrame("알림");
+      f.setSize(400, 300);
+      f.setLayout(new FlowLayout());
+      
+      
+      JPanel z = new JPanel();  
+      z.setPreferredSize(new Dimension(400, 20));  //사이즈
+      //z.setBackground(Color.green);  // 배경색
+      f.add(z);
+      
 
-		JLabel day = new JLabel(now_dt);
-		f.add(day);
+      JLabel day = new JLabel(now_dt);
+      z.add(day);
 
-		JPanel zp = new JPanel();
-		zp.setSize(300, 20);
-		JLabel b1 = new JLabel("오늘의 일정입니다! 일정을 확인해주세요 :) ");
-		f.add(b1);
+      
+      JLabel b1 = new JLabel("오늘의 일정입니다! 일정을 확인해주세요 :) ");
+      z.add(b1);
 
-		JLabel b2 = new JLabel("");
-		f.add(b2);
-		JLabel b3 = new JLabel("");
-		f.add(b3);
+      /*
+      JLabel b2 = new JLabel("");
+      f.add(b2);
+      
+      JLabel b3 = new JLabel("");
+      f.add(b3);
+*/
+      
+      JPanel z2 = new JPanel();  
+      z2.setPreferredSize(new Dimension(400, 170));  //사이즈
+      //z2.setBackground(Color.blue);  // 배경색
+      f.add(z2);
+      
+      
+      JTextArea tf = new JTextArea();
+      tf.setFont(new Font("Arial-Black",Font.PLAIN,14));
+      //tf.setBounds(10,10,380,250);
+      z2.add(tf, "Center");
+      
+      
+      try {
+         String driver = "oracle.jdbc.driver.OracleDriver";
+         String url = "jdbc:oracle:thin:@localhost:1521/xe";
+         String user = "c##green";
+         String password = "green1234";
+         Class.forName(driver);
+         con = DriverManager.getConnection(url, user, password);
 
-		JCheckBox newCheckbox = new JCheckBox("오늘의 일정을 확인했습니다");
-		f.add(newCheckbox);
+         String selectQuery = "SELECT TD_DAY,TD_LIST FROM TODOLIST";
+         pstmt = con.prepareStatement(selectQuery);
+         rs = pstmt.executeQuery();
 
-		JButton bt1 = new JButton("확인");
-		f.add(bt1);
-		
-		JTextArea tf = new JTextArea();
-		tf.setFont(new Font("Arial-Black", Font.PLAIN, 14));
-		tf.setBounds(10, 10, 380, 250);
-		f.add(tf);
+         StringBuilder tasklist = new StringBuilder();
+         String alarmTime = null;
 
-		try {
-			String driver = "oracle.jdbc.driver.OracleDriver";
-			String url = "jdbc:oracle:thin:@localhost:1521/xe";
-			String user = "c##green";
-			String password = "green1234";
-			Class.forName(driver);
-			con = DriverManager.getConnection(url, user, password);
+         while (rs.next()) {
+            String task = rs.getString("TD_LIST");
+            alarmTime = rs.getString("TD_DAY");
+            tasklist.append(task).append("\n");
+         }
+         if (tasklist.length() > 0) {
+            tf.setText(tasklist.toString());
+            
+            JOptionPane.showMessageDialog(f, "사용자의 리스트를 불러왔습니다.");
+         } else {
+            JOptionPane.showMessageDialog(f, "사용자의 리스트가 없습니다.");
+         }
+         con.close();
 
-			String selectQuery = "SELECT TD_LIST FROM TODOLIST";
-			pstmt = con.prepareStatement(selectQuery);
-			rs = pstmt.executeQuery();
+         final String finalAlarmTime = alarmTime;
+         
+         
+      } catch (Exception e1) {
+         e1.printStackTrace();
+      }
+      
+      
+      JPanel z3 = new JPanel();  
+      z3.setPreferredSize(new Dimension(400, 50));  //사이즈
+      //z3.setBackground(Color.red);  // 배경색
+      f.add(z3);
+      
+      
+      JCheckBox newCheckbox = new JCheckBox("오늘의 일정을 확인했습니다");
+      z3.add(newCheckbox);
 
-			StringBuilder tasklist = new StringBuilder();
-			String alarmTime = null;
+      JButton bt1 = new JButton("확인");
+      z3.add(bt1);
+      
 
-			while (rs.next()) {
-				String task = rs.getString("TD_LIST");
-				tasklist.append(task).append("\n");
-			}
-			if (tasklist.length() > 0) {
-				tf.setText(tasklist.toString());
+      bt1.addActionListener(new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            boolean isChecked = newCheckbox.isSelected();
+            if (isChecked) {
+               f.dispose();
+            } 
+         }
+      });
+      
+      
 
-				JOptionPane.showMessageDialog(f, "사용자의 리스트를 불러왔습니다.");
-			} else {
-				JOptionPane.showMessageDialog(f, "사용자의 리스트가 없습니다.");
-			}
-			con.close();
+      f.setResizable(false);
+      f.setVisible(true);
 
-			bt1.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-					boolean isChecked = newCheckbox.isSelected();
-					if (isChecked) {
-						f.dispose();
-					}
+   }
 
-				}
 
-			});
-		} catch (Exception e1) {
-			e1.printStackTrace();
-		}
 
-		f.setResizable(false);
-		f.setVisible(true);
+   public static void main(String[] args) {
+      new Tell();
 
-	}
+   }
 
-	public static void main(String[] args) {
-		new Tell();
+   @Override
+   public void itemStateChanged(ItemEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void itemStateChanged(ItemEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void actionPerformed(ActionEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void mouseClicked(MouseEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void mousePressed(MouseEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void mouseReleased(MouseEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void mouseEntered(MouseEvent e) {
+      // TODO Auto-generated method stub
 
-	}
+   }
 
-	@Override
-	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
+   @Override
+   public void mouseExited(MouseEvent e) {
+      // TODO Auto-generated method stub
 
-	}
-
-	@Override
-	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
-
-	}
+   }
 
 }
